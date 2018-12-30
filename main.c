05c64d9b437c0f026ea7ba8fce1a95daf1ae5749
Igor Torrente <igortorrente@hotmail.com>
2018-12-30 20:05:21 -0200

    Fixes a large number of comments and adds Upper case file format

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
 #include <time.h>
 #include <getopt.h>
 
+// Image position on vector of images
 enum{ cover1 = 0, cover2, secret };
+// Mode
 enum{ undefined = 1, decrypt, encrypt };
 enum{ width = 0, height };
 enum{ none };
@@ -20,20 +22,20 @@ typedef struct imageData{
 } imageData;
 
 // TODO: Check if this name make any sense
-// Map number in range 0-255 to 0-99
+// Maps a number on from range of 0-255 on 0-99
 #define reduce(image) (lround((99.0 / 255.0) * (image)))
 
-// Get ten and unit of a number
+// Gets ten and unit of a number
 #define ten(number) ((int8_t)((number) % 100))
 
-// Retrieve a unit of a channeel
+// Retrieves a unit of a channeel
 #define retrieveUnit(cover1, cover2) (((ten(cover1) / 10)) - ((cover2) % 10))
 
 // TODO: Change function rand to another one
-// Gereate a random number between 0-9
+// Generates a random number between 0-9
 #define randomUnit ((BYTE) (rand() / (RAND_MAX / 10)))
 
-// Adjust of the tone of color of pixel
+// Adjusts of the tone of color of pixel
 #define TONE_ADJUST(coverPixelHundred, coverPixelTen, rand) \
     if ((coverPixelHundred) >= 200 && ((coverPixelTen) + (rand) > 55)){ \
         (coverPixelHundred) = 100; \
@@ -45,7 +47,7 @@ typedef struct imageData{
         (coverPixelHundred) = 0; \
     } \
 
-// Verify if the resolution of images are not the same
+// Verifies if the resolution of images are not the same
 #define RESOLUTION_NOT_EQUAL(image1, image2) \
         images[image1].width != images[image2].width || \
         images[image1].height != images[image2].height
@@ -56,6 +58,11 @@ typedef struct imageData{
 #define JPEG 'gepj'
 #define BMP  '\0pmb'
 #define TGA  '\0agt'
+#define C_PNG  '\0GNP'
+#define C_JPG  '\0GPJ'
+#define C_JPEG 'GEPJ'
+#define C_BMP  '\0PMB'
+#define C_TGA  '\0AGT'
 
 // Inicialization of long options of opt
 #define LONG_OPTIONS \
@@ -69,6 +76,7 @@ typedef struct imageData{
         {0, 0, 0, 0} \
     }
 
+// Message of --help option
 #define HELP_MESSAGE \
     "Split Pixel 0.2 (pre-alpha)\n" \
     "Program based on Leandro Ribeiro's algorithm which allows to hide one image " \
@@ -101,51 +109,69 @@ typedef struct imageData{
     "Project repository: https://github.com/Igortorrente/Split-Pixel\n" \
     "Paper: <Leandro's Papers Here!!>\n"
 
-int imageFormat(char* imageName){
-    int imageFormat = 0;
+// Function that identifier of image format
+// Receive a string with a path of image, including image name itself
+// Returns a int that represent image format
+uint32_t imageFormat(char* imageName){
+    uint32_t imageFormat = none;
+    
+    // Get a pointer to latest '.'
     const char* formatPointer = strrchr(imageName, '.');
 
-    if (formatPointer != NULL){
+    // Check if some '.' was found
+    if (formatPointer != none){
         memcpy(&imageFormat, formatPointer + 1, 4 * sizeof(char));
     }
 
     return imageFormat;
 }
 
+// This function identify and loads a image 
+// Receives a imageData struct 
+// Returns a pointer to image
 FIBITMAP* loadImage(imageData* image){
     switch(imageFormat(image->currentplace)){
-        case PNG:
+        case PNG: case C_PNG:
             return FreeImage_Load(FIF_PNG, image->currentplace, PNG_IGNOREGAMMA);
-        case JPEG: case JPG:
+        case JPEG: case JPG: case C_JPEG: case C_JPG:
             return FreeImage_Load(FIF_JPEG, image->currentplace, JPEG_ACCURATE);
-        case BMP:
+        case BMP: case C_BMP:
             return FreeImage_Load(FIF_BMP, image->currentplace, BMP_DEFAULT);
-        case TGA:
+        case TGA: case C_TGA:
             return FreeImage_Load(FIF_TARGA, image->currentplace, TARGA_LOAD_RGB888);
+        // Case '.' not found
         default:
             return NULL;
     }
 }
 
-// Function to save images in different formats
-int writeImage(imageData* image){
+// Function to save images based on format
+// Receives a imageData struct 
+// Returns the result of write
+int8_t writeImage(imageData* image){
+    // Based on identifier save image on correct format
     switch (imageFormat(image->newplace)){
-        case PNG:
+        case PNG: case C_PNG:
             return !FreeImage_Save(FIF_PNG, image->pixelPointer, image->newplace, PNG_Z_NO_COMPRESSION);
-        case JPEG: case JPG:
+        case JPEG: case JPG: case C_JPEG: case C_JPG:
             return !FreeImage_Save(FIF_JPEG, image->pixelPointer, image->newplace, JPEG_QUALITYSUPERB);
-        case BMP:
+        case BMP: case C_BMP:
             return !FreeImage_Save(FIF_BMP, image->pixelPointer, image->newplace, BMP_DEFAULT);
-        case TGA:
+        case TGA: case C_TGA:
             return !FreeImage_Save(FIF_TARGA, image->pixelPointer, image->newplace, TARGA_DEFAULT);
-        case none:
+        // Case '.' not found
+        case none: 
             return -2;
+        // Case format not supported
         default:
             return -1;
     }
 }
 
-int saveImage(imageData* images, int position){
+// Function to save a image, and if an error occurred, print a error message
+// Receives a imageData struct
+// Returns the result of write
+int8_t saveImage(imageData* images, uint8_t position){
     switch (writeImage(&images[position])){
         case -2:
             fprintf(stderr, "Error: Missing format of image %d\n", position + 1);
@@ -162,8 +188,10 @@ int saveImage(imageData* images, int position){
     }
 }
 
-// Resize a image function
-int resizeImages(imageData* image, uint32_t height, uint32_t width){
+// Resizes an image with a new height and width
+// Receives a imageData struct, and two integers, a height and a width
+// Returns a error 
+int8_t resizeImages(imageData* image, uint32_t height, uint32_t width){
     FIBITMAP* resizedImage;
     resizedImage = FreeImage_Rescale(image->pixelPointer, width, height, FILTER_BSPLINE);
     if (resizedImage == NULL){
@@ -180,6 +208,7 @@ int resizeImages(imageData* image, uint32_t height, uint32_t width){
 
 int main(const int argc, const char* argv[], const char* env_var_ptr[]){
 
+    // Gets all inputs of program
     #ifdef DEBUG
         for (int i = 1; i < argc; ++i){
             printf("%d: %s\n", i, argv[i]);
@@ -189,17 +218,19 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
         }
         printf("\n\n");
     #endif
+    // Image vector, with space for each image
     imageData images[3];
-    uint32_t newResolution[2] = { 0, 0 }; // inputCount = 0, outputCount = 0;
+    uint32_t newResolution[2] = { none, none };
+    // Initializes mode
     uint8_t mode = undefined;
+    // Initializes srand
     srand((unsigned int) time(NULL));
-
-    //FreeImage_SetOutputMessage(imageErrorHandler);
 
     struct option longOptions[] = LONG_OPTIONS;
 
+    // Parser of options
     int option = 0;
-    while ((option = getopt_long(argc, (char* const*) argv, "m:r:i:o:hv", longOptions, NULL)) != -1){
+    while ((option = getopt_long(argc, (char *const *)argv, "m:r:i:o:hv", longOptions, NULL)) != -1){
         switch (option){
             case 'i':
                 // Get input addresses
@@ -220,8 +251,11 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
                 printf("This suppose to be a verbose mode, not implemented yet :(\n");
                 break;
             case 'r':
+                // Get values of -r option
                 for (int i = -1; i < 1; i++){
+                    // Convert to int the value
                     newResolution[i + 1] = atoi(argv[optind + i]);
+                    // If 0 or error 
                     if (!newResolution[i + 1]){
                         fprintf(stderr, "Error:'%s' is not valid value for resolution of image\n", argv[optind + i]);
                         return 1;
@@ -229,6 +263,7 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
                 }
                 break;
             case 'm':
+                // Discover mode
                 if (!strcmp("encrypt", optarg)){
                     mode = encrypt;
                 } else if (!strcmp("decrypt", optarg)){
@@ -239,6 +274,7 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
                 }
                 break;
             case '?':
+                // flush opt message
                 fflush(stderr);
                 printf("Try --help\n");
                 return 1;
@@ -250,11 +286,14 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
         return 1;
     }
 
+    // Initialize Free Image without plugin
     FreeImage_Initialise(FALSE);
 
-    // Load input images
+    // Loads input images
     for (uint8_t i = cover1; i < mode; i++){
+        // Loads the image
         images[i].pixelPointer = loadImage(&images[i]);
+        // Checks if image has been loaded
         if (images[i].pixelPointer == NULL){
             fprintf(stderr, "Error: Can't open image from '%s'\n", images[i].currentplace);
             return 1;
@@ -262,6 +301,7 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
 
         FIBITMAP* image = images[i].pixelPointer;
         images[i].BPP = FreeImage_GetBPP(image);
+        //Checks if Bits Per Pixel(BPP) of the image are supported
         switch(images[i].BPP){
             case 1: case 4: case 16:
                 fprintf(stderr, "Image with %d bit(s) per channels not supported\n", images[i].BPP);
@@ -270,6 +310,7 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
                 break;
         }
 
+        // Updates metadata of image
         images[i].height = FreeImage_GetHeight(image);
         images[i].width = FreeImage_GetWidth(image);  
         images[i].channels = (FreeImage_GetLine(image) / images[i].width);
@@ -278,17 +319,22 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
             images[i].channels, FreeImage_GetBPP(images[i].pixelPointer));
     }
 
+    // Checks if secret image is grayscale 
     if(images[secret].BPP == 8){
-        for (uint8_t image = 0; image <= cover2; ++image){
+        // Converts cover 1 and 2 to grayscale
+        for (uint8_t image = cover1; image <= cover2; ++image){
             images[image].pixelPointer = FreeImage_ConvertToGreyscale(images[image].pixelPointer);
             images[image].BPP = 8;
             images[image].channels = 1;
         }
     } else{
+        // Checks if secret image has alpha
         uint8_t limit = cover2;
-        if(images[secret].BPP == 32)
+        if(images[secret].BPP == 32){
             limit = secret;
-        for (uint8_t image = 0; image <= limit; ++image){
+        }
+        // Removes alpha channel of images 
+        for (uint8_t image = cover1; image <= limit; ++image){
             images[image].pixelPointer = FreeImage_ConvertTo24Bits(images[image].pixelPointer);
             images[image].BPP = 24;
             images[image].channels = 3;
@@ -296,7 +342,7 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
     }
 
     if(mode == decrypt){
-        // Fix some values and initialize output images
+        // Fixes some values and initialize output images
         images[secret] = images[cover1];
         images[cover1].newplace = NULL;
         images[secret].currentplace = NULL;
@@ -306,15 +352,17 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
             return 1;
         }
 
-        // Allocate matrix of recovered image
+        // Allocates a matrix for recovered image
         images[secret].pixelPointer = FreeImage_Allocate(images[secret].width, 
             images[secret].height, 24, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK);
+        // Check if allocation has successful
         if (images[secret].pixelPointer == NULL){
             fprintf(stderr, "Error: Couldn't allocate image matrix\n");
             return 1;
         }
 
         // Decrypt for
+        // get line size in bytes and height of image
         uint64_t line = images[secret].width * images[secret].channels;
         uint32_t imageHeight = images[cover1].height;
         for (uint32_t i = 0; i < imageHeight; ++i){
@@ -340,20 +388,22 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
         }
     } else if(mode == encrypt){
 
+        // Initializes the newplace
         images[secret].newplace = NULL;
 
-        // Verify if resize is required
+        // Verifies if resize was required
         if (newResolution[0] != none){
+            // Resize all images
             for (int cover = cover1; cover <= secret; ++cover){
                 if (resizeImages(&images[cover], newResolution[height], newResolution[width])){
                     return 1;
                 }                
             }
         } else{
-            // Verify if resolution of cover1 is different of secret(Hidden) image
+            // Verify if resolution of cover is different of secret(Hidden) image
             for (int cover = cover1; cover <= cover2; ++cover){
                 if (RESOLUTION_NOT_EQUAL(cover, secret)){
-                    // Resize cover1 to resolution of image secret(Hidden)
+                    // Resize cover to resolution of image secret(Hidden)
                     if (resizeImages(&images[cover], images[secret].height, images[secret].width)){
                         fprintf(stderr, "Error: Couldn't resize cover image %d\n", cover);
                         return 1;
@@ -370,9 +420,10 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
             BYTE* cover2Pointer = (BYTE*)FreeImage_GetScanLine(images[cover2].pixelPointer, i);
             BYTE* secretPointer = (BYTE*)FreeImage_GetScanLine(images[secret].pixelPointer, i);
             for (uint64_t j = 0; j < line; j++){
+                // Reduces the value of channel
                 BYTE reducedChannel = (BYTE) reduce(secretPointer[j]);
 
-                // Get ten and unit of secret(Hidden) image
+                // Gets ten and unit of secret(Hidden) image
                 BYTE originalUnit = (BYTE) (reducedChannel % 10);
                 BYTE originalTen = (BYTE) (reducedChannel / 10);
                 // Generates a ten and unit random number
@@ -382,15 +433,19 @@ int main(const int argc, const char* argv[], const char* env_var_ptr[]){
                 BYTE coverPixelHundred = cover1Pointer[j];
                 BYTE coverPixelTen = (BYTE) (10 * ((randTen + originalTen) % 10));
 
+                // Adjusts tone of channel of cover 1
                 TONE_ADJUST(coverPixelHundred, coverPixelTen, randUnit);
 
+                // Saves the new value of channel on cover1
                 cover1Pointer[j] = coverPixelHundred + coverPixelTen + randUnit;
 
                 coverPixelHundred = cover2Pointer[j];
                 coverPixelTen = (BYTE) (10 * ((randUnit + originalUnit) % 10));
-
+                
+                // Adjusts tone of channel of cover 2
                 TONE_ADJUST(coverPixelHundred, coverPixelTen, randTen);
-
+                
+                // Saves the new value of channel on cover 2
                 cover2Pointer[j] = (coverPixelHundred + coverPixelTen + randTen);
             }
         }
